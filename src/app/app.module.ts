48e3dfa4fb8cc3e6a37f359b92a0c0ed073eca31
Nikita Kobylkin <sheliss.kai@gmail.com>
2022-07-05 15:34:53 +0300

    template and styling

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { MenuComponent } from './components/menu/menu.component';
+import { ButtonComponent } from './components/button/button.component';
+import { FieldComponent } from './components/field/field.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    MenuComponent,
+    ButtonComponent,
+    FieldComponent
   ],
   imports: [
     BrowserModule,
